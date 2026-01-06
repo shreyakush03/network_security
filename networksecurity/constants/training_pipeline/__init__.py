b1fb96386dd1aaba5da6f989c05f18dd12804a12
shreyakush03 <shreyakush013@gmail.com>
2026-01-06 21:07:13 +0530

    data ingestion

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,8 @@
+import os 
+import sys
+import pandas as pd
+import numpy as np
+
+DATA_INGESTION_COLLECTION_NAME:str="NetworkData"
+DATA_INGESTION_DATABASE_NAME:str="SHREYAKUSH"
+DATA_INGESTION_DIR_NAME:str="data_ingestion"
