b149a95af04c374b6ecf224397dcb7a1e9abdcaa
shreyakush03 <shreyakush013@gmail.com>
2026-01-12 21:07:22 +0530

    data validation

---
+++ networksecurity/components/data_validation.py
@@ -0,0 +1,22 @@
+from networksecurity.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.exception.exception import NetworkSecurityException 
+from networksecurity.logging.logger import logging
+from networksecurity.constants.training_pipeline import SCHEMA_FILE_PATH
+from networksecurity.utils.main.utils import read_yaml_file
+from scipy.stats import ks_2samp
+import pandas as pd 
+import numpy as np
+import os,sys
+
+class DataValidation:
+    def __init__(self,data_ingestion_artifact:DataIngestionArtifact,
+                 data_validation_config:DataValidationConfig):
+        try:
+            self.data_ingestion_artifact=data_ingestion_artifact
+            self.data_validation_config=data_validation_config
+            self._schema_config=read_yaml_file(SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
